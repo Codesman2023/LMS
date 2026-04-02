31b727d13977fc285c33e7645ef0184566bb25ca
Tanuj <152773375+Codesman2023@users.noreply.github.com>
2026-04-02 20:28:49 +0530

    fixed deplyed bug-2

--- components/public/BlogListClient.jsx
+++ components/public/BlogListClient.jsx
@@ -91,7 +91,7 @@ export default function BlogListClient({ blogs }) {
                   href={`/blogpost/${blog.slug}`}
                   className="mt-auto inline-flex items-center justify-center rounded-md border px-4 py-2 text-sm font-medium transition hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black"
                 >
-                  Read More ->
+                  Read More &rarr;
                 </Link>
               </div>
             </div>
